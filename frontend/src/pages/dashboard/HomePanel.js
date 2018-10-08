3dee3c27e08a909319b6a27e811493ace6971ac7
Dan Florio <dflo@danflorio.com>
2018-10-07 23:34:31 -0400

    restyle dashboard home

--- frontend/src/pages/dashboard/HomePanel.js
+++ frontend/src/pages/dashboard/HomePanel.js
@@ -11,40 +11,58 @@ const Panel = (props) => {
   const { num, text, icon } = props;
   return (
     <div className="panel">
-      <div className="panel__inner">
-        <FontAwesome
-          className="panel__icon"
-          icon={icon}
-        />
-        <div className="panel__text">
-          {num} {text}
-        </div>
+      <FontAwesome
+        className="panel__icon"
+        icon={icon}
+      />
+      <div className="panel__text">
+        {num} {text}
       </div>
     </div>
   );
 }
 
-const HomePanel = () => (
-  <div className="dashboard-home">
-    <Panel
-      num={12}
-      text="Songs"
-      icon={['fas', 'music']}
-    />
-    <Panel
-      num={3}
-      text="Binders"
-      icon={['fas', 'book']}
-    />
+const HomePanel = (props) => (
+  <div className="home-panel">
+    <h1 className="home-panel__user" >{props.user.username}</h1>
+    <div className="home-panel__block home-panel__block--songs">
+      <div className="home-panel__block-overlay"></div>
+      <FontAwesome
+        className="home-panel__block-icon"
+        icon={['fas', 'music']}
+      />
+      <div className="home-panel__block-text">
+        12 Songs
+      </div>
+    </div>
+    <div className="home-panel__block home-panel__block--binders">
+    <div className="home-panel__block-overlay"></div>
+      <FontAwesome
+        className="home-panel__block-icon"
+        icon={['fas', 'book']}
+      />
+      <div className="home-panel__block-text">
+        3 Binders
+      </div>
+    </div>
   </div>
 );
 
 class HomePanelContainer extends React.Component {
   render() {
     return (
-      <HomePanel />
+      <HomePanel user={this.props.user} />
     );
   }
 }
 
-export default HomePanelContainer;
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(HomePanelContainer);
