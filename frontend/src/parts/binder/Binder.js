e2e7276ac9fb262236f1638cb9368b272bac33f2
Dan Florio <dflo@danflorio.com>
2018-10-11 20:39:04 -0400

    fix binder loader and add keys to songs

--- frontend/src/parts/binder/Binder.js
+++ frontend/src/parts/binder/Binder.js
@@ -1,43 +1,47 @@
 // Third party
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 // Actions
-import { parseSong } from '../song/SongActions';
+import { parseSong } from "../song/SongActions";
 
 // Components
-import { DualRingLoader } from '../loaders/Loaders';
-import Song from '../song/Song';
+import { DualRingLoader } from "../loaders/Loaders";
+import Song from "../song/Song";
 
+// Assets
+import "./Binder.css";
 
-const Binder = (props) => {
-
+const Binder = props => {
   if (props.fetching) {
     return (
       <div className="binder">
-        <span className="binder__loader-text">
-          Loading song...
-        </span>
-        <DualRingLoader />
+        <div className="binder__loader">
+          <span className="binder__loader-text">Loading songs...</span>
+          <DualRingLoader />
+        </div>
       </div>
     );
   } else {
     return (
       <div className="binder">
-        {
-          props.binder.songs &&
+        {props.binder.songs &&
           props.binder.songs.map(song => (
-            <Song title={song.title} artist={song.artist} content={parseSong(song.content)} />
-          ))
-        }
+            <Song
+              key={song.id}
+              title={song.title}
+              artist={song.artist}
+              content={parseSong(song.content)}
+            />
+          ))}
       </div>
     );
   }
-}
+};
 
 Binder.propTypes = {
   binder: PropTypes.object.isRequired,
   fetching: PropTypes.bool.isRequired
-}
+};
 
 export default Binder;
--- frontend/src/parts/song/Song.js
+++ frontend/src/parts/song/Song.js
@@ -1,22 +1,18 @@
 // Third party
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 // Assets
-import './Song.css';
+import "./Song.css";
 
 // Components
-import { DualRingLoader } from '../loaders/Loaders';
+import { DualRingLoader } from "../loaders/Loaders";
 
-
-const Song = ({title, artist, content, fetching}) => {
-
+const Song = ({ title, artist, content, fetching }) => {
   if (fetching) {
     return (
       <div className="song__loader">
-        <span className="song__loader-text">
-          Loading song...
-        </span>
+        <span className="song__loader-text">Loading song...</span>
         <DualRingLoader />
       </div>
     );
@@ -25,38 +21,34 @@ const Song = ({title, artist, content, fetching}) => {
       <div className="song">
         <h3 className="song__title">{title}</h3>
         <h4 className="song__artist">{artist}</h4>
-        {
-          content.map((line, i) => {
-            return (
-              <div key={i} className="song__line">
-                {
-                  line.map((chunk, j) => {
-                    return (
-                      <span key={j} className="song__chunk">
-                        <span className="song__chord">
-                          {chunk.chord === "" ? " " : chunk.chord}
-                        </span>
-                        <span className="song__lyric">
-                          {chunk.lyric === "" ? " " : chunk.lyric}
-                        </span>
-                      </span>
-                    )
-                  })
-                }
-              </div>
-            )
-          })
-        }
+        {content.map((line, i) => {
+          return (
+            <div key={i} className="song__line">
+              {line.map((chunk, j) => {
+                return (
+                  <span key={j} className="song__chunk">
+                    <span className="song__chord">
+                      {chunk.chord === "" ? " " : chunk.chord}
+                    </span>
+                    <span className="song__lyric">
+                      {chunk.lyric === "" ? " " : chunk.lyric}
+                    </span>
+                  </span>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     );
   }
-}
+};
 
 Song.propTypes = {
   title: PropTypes.string.isRequired,
   artist: PropTypes.string.isRequired,
   content: PropTypes.array.isRequired,
-  fetching: PropTypes.bool.isRequired
-}
+  fetching: PropTypes.bool
+};
 
 export default Song;
