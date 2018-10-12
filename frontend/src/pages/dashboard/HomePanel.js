7200c2bf8ea76af77d22c9dd950f60864fcfc9b5
Dan Florio <dflo@danflorio.com>
2018-10-11 20:05:51 -0400

    link dashboard with songs and binders

--- frontend/src/pages/dashboard/HomePanel.js
+++ frontend/src/pages/dashboard/HomePanel.js
@@ -1,65 +1,56 @@
 // Third party
-import React from 'react';
-import { connect } from 'react-redux';
-import FontAwesome from '@fortawesome/react-fontawesome';
+import React from "react";
+import { connect } from "react-redux";
+import FontAwesome from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 // Assets
-import './HomePanel.css';
+import "./HomePanel.css";
 
-
-const Panel = (props) => {
+const Panel = props => {
   const { num, text, icon } = props;
   return (
     <div className="panel">
-      <FontAwesome
-        className="panel__icon"
-        icon={icon}
-      />
+      <FontAwesome className="panel__icon" icon={icon} />
       <div className="panel__text">
         {num} {text}
       </div>
     </div>
   );
-}
+};
 
-const HomePanel = (props) => (
+const HomePanel = props => (
   <div className="home-panel">
-    <h1 className="home-panel__user" >{props.user.username}</h1>
+    <h1 className="home-panel__user">{props.user.username}</h1>
     <div className="home-panel__block home-panel__block--songs">
-      <div className="home-panel__block-overlay"></div>
-      <FontAwesome
-        className="home-panel__block-icon"
-        icon={['fas', 'music']}
-      />
-      <div className="home-panel__block-text">
-        12 Songs
-      </div>
+      <div className="home-panel__block-overlay" />
+      <FontAwesome className="home-panel__block-icon" icon={["fas", "music"]} />
+      <div className="home-panel__block-text">12 Songs</div>
+      <Link className="home-panel__block-link" to="/dashboard/songs">
+        View Songs
+      </Link>
     </div>
     <div className="home-panel__block home-panel__block--binders">
-    <div className="home-panel__block-overlay"></div>
-      <FontAwesome
-        className="home-panel__block-icon"
-        icon={['fas', 'book']}
-      />
-      <div className="home-panel__block-text">
-        3 Binders
-      </div>
+      <div className="home-panel__block-overlay" />
+      <FontAwesome className="home-panel__block-icon" icon={["fas", "book"]} />
+      <div className="home-panel__block-text">3 Binders</div>
+      <Link className="home-panel__block-link" to="/dashboard/binders">
+        View Binders
+      </Link>
     </div>
   </div>
 );
 
 class HomePanelContainer extends React.Component {
   render() {
-    return (
-      <HomePanel user={this.props.user} />
-    );
+    return <HomePanel user={this.props.user} />;
   }
 }
 
 function mapStateToProps(state) {
   return {
     user: state.auth.user
-  }
+  };
 }
 
 export default connect(
--- frontend/src/parts/top-bar/TopBar.js
+++ frontend/src/parts/top-bar/TopBar.js
@@ -1,45 +1,39 @@
 // Third party
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import FontAwesome from '@fortawesome/react-fontawesome';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import FontAwesome from "@fortawesome/react-fontawesome";
 
 // Assets
-import './TopBar.css';
+import "./TopBar.css";
 
-
-const TopBar = ({sId}) => {
+const TopBar = ({ sId }) => {
   return (
     <div className="topbar">
-
-      <Link className="topbar__back" to="/binder">
+      <Link className="topbar__back" to="/dashboard">
         <FontAwesome
           className="topbar__back-arrow"
-          icon={['fas', 'angle-double-left']}
+          icon={["fas", "angle-double-left"]}
         />
         back to binder
       </Link>
-    
-      {
-        sId &&
+
+      {sId && (
         <div className="topbar__right-controls">
           <Link className="topbar__control" to={"/songs/edit/" + sId}>
             edit
           </Link>
-          <a
-            className="topbar__control"
-            href="javascript:window.print()">
-            <FontAwesome icon={['fas', 'print']} /> print
+          <a className="topbar__control" href="javascript:window.print()">
+            <FontAwesome icon={["fas", "print"]} /> print
           </a>
         </div>
-      }
-      
+      )}
     </div>
   );
-}
+};
 
 TopBar.propTypes = {
   sId: PropTypes.string
-}
+};
 
 export default TopBar;
